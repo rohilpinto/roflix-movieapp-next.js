b666034b317349de62b235bc949eed84f4ab0fee
Rohil pinto <rohil.pinto@qualyval.com>
2022-01-12 11:25:44 +0530

    removed font

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import { createTheme, ThemeProvider } from "@mui/material";
 import "../styles/globals.css";
-import "@fontsource/lato";
-import "@fontsource/lato/100.css";
-import "@fontsource/lato/300.css";
-import "@fontsource/lato/400.css";
-import "@fontsource/lato/700.css";
-import "@fontsource/lato/900.css";
+// import "@fontsource/lato";
+// import "@fontsource/lato/100.css";
+// import "@fontsource/lato/300.css";
+// import "@fontsource/lato/400.css";
+// import "@fontsource/lato/700.css";
+// import "@fontsource/lato/900.css";
 
 function MyApp({ Component, pageProps }) {
   const globalTheme = createTheme({
