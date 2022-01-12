b7f1c42dd4596684ff50ad52aabb5ed57520ead6
Rohil pinto <rohil.pinto@qualyval.com>
2022-01-12 11:35:25 +0530

    cssbaseline

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { createTheme, ThemeProvider } from "@mui/material";
-import "../styles/globals.css";
-// import "@fontsource/lato";
-// import "@fontsource/lato/100.css";
-// import "@fontsource/lato/300.css";
-// import "@fontsource/lato/400.css";
-// import "@fontsource/lato/700.css";
-// import "@fontsource/lato/900.css";
+// import "../styles/globals.css";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import "@fontsource/lato";
+import "@fontsource/lato/100.css";
+import "@fontsource/lato/300.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
+import "@fontsource/lato/900.css";
 
 function MyApp({ Component, pageProps }) {
   const globalTheme = createTheme({
@@ -25,13 +27,14 @@ function MyApp({ Component, pageProps }) {
     },
 
     typography: {
-      // fontFamily: ["lato"].join(","),
+      fontFamily: ["Lato"].join(","),
     },
   });
 
   return (
     <div>
       <ThemeProvider theme={globalTheme}>
+        <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
     </div>
